8e8b2252865efd0dbb44c10d3328bc471bf9aaed
skzfr17 <bayyinahtundwisumatri@gmail.com>
2025-05-26 15:44:43 +0800

    tes api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,11 @@
 from fastapi.testclient import TestClient
 from backend.main import app 
 
-
 client = TestClient(app)
 
 def test_get_tasks():
-    response = client.get("/all-tasks")
+    token = "azy34pcfkef4pr489wq5d4w7nyfe8z"  # Ganti dengan token yang valid untuk test
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.get("/all-tasks", headers=headers)
     assert response.status_code == 200
     assert isinstance(response.json(), list)
