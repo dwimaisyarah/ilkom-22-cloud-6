d2307f41778726795ab41ec34e176f0f87a5f844
Kumaladwi <dkumala354@gmail.com>
2025-06-28 13:23:06 +0800

    tambah halaman login dan signup, serta konfigurasi user

--- Percobaan Lala/src/controller/users.singup.js
+++ Percobaan Lala/src/controller/users.singup.js
@@ -6,7 +6,9 @@ export const userSignUp = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) throw new ApiError(401, 'Provide all fields', '');
+
     const hashedPassword = await bcrpyt.hash(password, 10);
+    
     const user = await User.create({
         name,
         email,
