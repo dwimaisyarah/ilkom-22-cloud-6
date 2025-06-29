49f3c4078138062ea2d0a8a003b26328e0bf0412
Selin Rahmadani <selinrahmadani91@gmail.com>
2025-06-29 20:13:03 +0800

    menambahkan fitur warna

---
+++ resources/constants/colors.js
@@ -0,0 +1,8 @@
+// List of color constants
+
+const COLORS = [
+    "red", "green", "blue", "yellow", "purple", "orange", "black", "white", "gray", "pink",
+    // Tambahkan hingga ratusan atau ribuan entri jika perlu
+];
+
+module.exports = COLORS;
