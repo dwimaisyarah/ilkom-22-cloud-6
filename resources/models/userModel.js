e3855c3fc2eecc0ff7378bcbc2784f97d7586544
Selin Rahmadani <selinrahmadani91@gmail.com>
2025-06-29 20:56:11 +0800

    mengupdate kode di userModel.js

--- resources/models/userModel.js
+++ resources/models/userModel.js
@@ -1,14 +1,38 @@
 // User model example
 
 class User {
-    constructor(id, name, email) {
+    constructor(id, name, email, role = 'user', isActive = true) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.role = role;
+        this.isActive = isActive;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
     }
 
     getDisplayName() {
         return `${this.name} <${this.email}>`;
+    }
+
+    activate() {
+        this.isActive = true;
+        this.updatedAt = new Date();
+    }
+
+    deactivate() {
+        this.isActive = false;
+        this.updatedAt = new Date();
+    }
+
+    updateProfile({ name, email }) {
+        if (name) this.name = name;
+        if (email) this.email = email;
+        this.updatedAt = new Date();
+    }
+
+    hasRole(role) {
+        return this.role === role;
     }
 }
 
