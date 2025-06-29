9a8681243a64402e2e5038fda2c99e4a9f0a8cca
Selin Rahmadani <selinrahmadani91@gmail.com>
2025-06-29 20:46:53 +0800

    menambahkan konfigurasi aplikasi

---
+++ resources/config/appConfig.js
@@ -0,0 +1,11 @@
+// Konfigurasi aplikasi
+
+const config = {
+    appName: "ResourceApp",
+    version: "1.0.0",
+    author: "Tim Pengembang",
+    environment: process.env.NODE_ENV || "development",
+    maxItems: 1000
+};
+
+module.exports = config;
