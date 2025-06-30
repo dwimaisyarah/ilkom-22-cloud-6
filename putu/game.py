d90895ecededf5cdc90527544b5c50b082ebf4b2
lindaandiari <lindaputu973@gmail.com>
2025-06-30 13:50:27 +0700

    Cek tebakan dan kurangi nyawa jika salah

--- putu/game.py
+++ putu/game.py
@@ -7,3 +7,11 @@ attempts = 10
 while attempts > 0:
     print("\nCurrent word: " + ' '.join(guessedWord))
     guess = input("Guess a letter: ").lower()
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedWord[i] = guess
+        print("Great guess!")
+    else:
+        attempts -= 1
+        print("Wrong guess! Attempts left: " + str(attempts))
