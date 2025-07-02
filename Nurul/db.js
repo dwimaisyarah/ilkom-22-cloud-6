779a87d68e3ba369224fc4de7a40ebdac0cbd5d6
nurulkhofifah29 <nurulkhofifah076@gmail.com>
2025-07-02 12:09:51 +0800

    menambahkan db.js

--- Nurul/db.js
+++ Nurul/db.js
@@ -1,12 +1,19 @@
-import {DBNAME} from '../constants.js';
-
+import { DBNAME } from '../constants.js';
 import mongoose from 'mongoose';
 
-export const connectToDB = async () =>{
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DBNAME}`);
-        console.log(connectionInstance.connection.host);
-    } catch (error) {
-        console.log(error);
-    }
+export const connectToDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DBNAME}`);
+
+    console.log(` Connected to MongoDB at host: ${connectionInstance.connection.host}`);
+    console.log(` Database Name: ${connectionInstance.connection.name}`);
+    console.log(` Connected at: ${new Date().toLocaleString()}`);
+
+    return connectionInstance;
+
+  } catch (error) {
+    console.error("Failed to connect to MongoDB");
+    console.error(error.message);
+    process.exit(1);
+  }
 };
