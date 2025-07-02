c8bfa358152966316fbb181bf76039299b1a7456
nurulkhofifah29 <nurulkhofifah076@gmail.com>
2025-07-02 13:07:44 +0800

    menambahkan file colors.js

---
+++ Nurul/colors.js
@@ -0,0 +1,16 @@
+// List of color constants
+
+const COLORS = [
+    "red", "green", "blue", "yellow", "purple", "orange", "black", "white", "gray", "pink",
+    // Tambahkan hingga ratusan atau ribuan entri jika perlu
+];
+
+// Kode tambahan di bawah ini
+// (Silakan tambahkan kode yang Anda maksud di sini)
+
+COLORS.push(
+    "cyan", "magenta", "lime", "maroon", "navy", "olive", "teal", "aqua", "fuchsia", "silver",
+    "gold", "beige", "coral", "indigo", "ivory", "khaki", "lavender", "mint", "plum", "salmon"
+);
+
+module.exports = COLORS;
